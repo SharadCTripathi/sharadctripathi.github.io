f3ca2d6f4837fadae940919a5275d5bf81f1855b
Dr. Sharad Chandra Tripathi <drsharadctripathi@gmail.com>
2026-03-07 12:50:03 +0530

    Handle undefined values in citation data
    Add default values for citations, hindex, and papers.

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ fetch("scholar.json")
 .then(response => response.json())
 .then(data => {
 
-document.getElementById("citations").innerText = data.citations;
-document.getElementById("hindex").innerText = data.hindex;
-document.getElementById("papers").innerText = data.papers;
+document.getElementById("citations").innerText = data.citations || 0;
+
+document.getElementById("hindex").innerText = data.hindex || 0;
+
+document.getElementById("papers").innerText = data.papers || 0;
 
 });
